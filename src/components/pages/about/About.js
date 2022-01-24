3611785837ecac5a6f0e082158aa09b85065229e
chrissyy99 <43761702+IslipAI@users.noreply.github.com>
2022-01-24 16:37:47 -0500

    Changed resume url

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -14,7 +14,7 @@ class About extends Component{
                         <br/>Full name is Alexander Christopher Islip.
                         <br/>A young professional who is trying to futher his career. 
                         <br/> Has a strong passion for computer science and software development.</h6>
-                        <h5 className="resume">Here's my <a href="https://github.com/IslipAI/resume/blob/master/Resume%20-%20Alexander%20Islip.pdf">resume</a></h5>
+                        <h5 className="resume">Here's my <a href="https://github.com/IslipAI/resume/blob/master/Resume%20-%20Alexander_Islip.pdf">resume</a></h5>
                     </div>
                 </div>
             </div>
